2c715ed84200b73378826ad289e783967f57640c
adam-s-k-i <adam.henne@gmail.com>
2026-08-26 22:49:48 +0200

    test: raise vitest timeout to 15s to fix flaky windows e2e tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,2 +1,7 @@
 import { defineConfig } from 'vitest/config';
-export default defineConfig({ test: { include: ['test/**/*.test.ts'] } });
+export default defineConfig({
+  test: {
+    include: ['test/**/*.test.ts'],
+    testTimeout: 15000,
+  },
+});
